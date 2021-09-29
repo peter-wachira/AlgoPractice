4d9946e9321293de591979dcdc7f510bc7ca0289
PeteHack <pwachira900@gmail.com>
2021-09-29 15:43:33 +0300

    implementing a linkedList

---
+++ src/linkedlists/LinkedList.java
@@ -0,0 +1,35 @@
+package linkedlists;
+
+import javax.sound.midi.Soundbank;
+
+public class LinkedList {
+    Node head;
+
+    public void insert(int data){
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+
+        if (head== null){
+            head = node;
+        }else {
+            //transversing between nodes
+            Node n = head;
+            while (n.next!= null){
+                n = n.next;
+            }
+            n.next = node;
+        }
+    }
+
+
+
+    public void show(){
+        Node node = head;
+        while ( node.next!= null){
+            System.out.println(node.data);
+            node = node.next;
+        }
+        System.out.println(node.data );
+    }
+}
