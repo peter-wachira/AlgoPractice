a288ba20b361b7867c35fc553a1962a0b616953b
PeteHack <pwachira900@gmail.com>
2021-10-04 15:20:15 +0300

    implementing stack using dynamic array

--- src/stack/Runner.java
+++ src/stack/Runner.java
@@ -4,15 +4,33 @@ package stack;
 public class Runner {
 
     public static void main(String[] args) {
-        Stack stack = new Stack();
+        DynamicStack stack = new DynamicStack();
         stack.push(12);
-        stack.push(13);
-        stack.push(10);
-        stack.push(17);
-
-        System.out.println("Pop \n"+ stack.pop());
-        System.out.println("Peek  \n \t"+ stack.peek());
-
         stack.show();
+        stack.push(13);
+        stack.show();
+        stack.push(10);
+        stack.show();
+        stack.push(17);
+        stack.show();
+        stack.push(17);
+        stack.show();
+        stack.push(17);
+        stack.show();
+        stack.push(17);
+        stack.show();
+        stack.push(17);
+        stack.show();
+
+//        System.out.println("Pop "+ stack.pop()+"\n");
+//        System.out.println("Peek "+ stack.peek());
+//        System.out.println("Size is "+ stack.size());
+//        System.out.println("Empty status "+stack.isEmpty());
+//        stack.show();
     }
+
+
+
+
+
 }
--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,35 +1,50 @@
 package stack;
 
-
 public class Stack {
 
-    int  stack[] = new int[5];
+
+
+    /*Implementing stack using fixed size Array*/
+
+    int[] stack = new int[5];
     //define the top of the stack
     int top = 0;
 
-    public  void push (int data){
-        stack[top] = data;
-        top ++;
+    public void push(int data) {
+        if (top == 5) {
+            System.out.println("Stack is full");
+        } else {
+            stack[top] = data;
+            top++;
+        }
     }
-
-    public int pop(){
-        int data;
-        top--;
-        data = stack[top];
-        stack[top] = 0;
+    public int pop() {
+        int data = 0;
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+        } else {
+            top--;
+            data = stack[top];
+            stack[top] = 0;
+        }
         return data;
     }
-
-    public int peek(){
+    public int peek() {
         int data;
-        data = stack[top-1];
+        data = stack[top - 1];
         return data;
+    }
+    public int size() {
+        return top;
+    }
+    public boolean isEmpty() {
+        return top <= 0;
     }
 
 
-    public  void show(){
-        for (int n : stack){
-            System.out.println(n+ " ");
+    public void show() {
+        for (int n : stack) {
+            System.out.println(n + " ");
         }
     }
 }
